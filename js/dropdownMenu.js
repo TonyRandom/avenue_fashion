e33a112225052c8fc0e14587103eba7be4aa8428
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2020-08-20 14:33:18 +0300

    brand page started

--- js/dropdownMenu.js
+++ js/dropdownMenu.js
@@ -1,10 +1,12 @@
 var mensMenu = document.querySelector(".mens_menu");
 var womensMenu = document.querySelector(".womens_menu");
 var localStoresMenu = document.querySelector(".local_stores_menu");
+var lookBookMenu = document.querySelector(".look_book_menu");
 
 var menDropDown = document.querySelector(".mens_dropdown_menu");
 var womenDropDown = document.querySelector(".womens_dropdown_menu");
 var localStoresDropDown = document.querySelector(".local_stores_dropdown_menu");
+var lookBookDropDown = document.querySelector(".look_book_dropdown_menu");
 
 
 
@@ -13,6 +15,7 @@ mensMenu.addEventListener("click", function (evt) {
     evt.preventDefault();
     localStoresDropDown.classList.remove("local_stores_dropdown_menu_show");
     womensMenu.classList.remove("womens_dropdown_menu_show");
+    localStoresMenu.classList.remove("local_stores_dropdown_menu");
     menDropDown.classList.toggle("mens_dropdown_menu_show");
     }); 
 
@@ -22,6 +25,7 @@ womensMenu.addEventListener("click", function (evt) {
         
     localStoresDropDown.classList.remove("local_stores_dropdown_menu_show");
     menDropDown.classList.remove("mens_dropdown_menu_show");
+    localStoresMenu.classList.remove("local_stores_dropdown_menu");
     womenDropDown.classList.toggle("womens_dropdown_menu_show");
      
         
@@ -35,6 +39,13 @@ localStoresMenu.addEventListener("click", function (evt) {
     localStoresDropDown.classList.toggle("local_stores_dropdown_menu_show");
         }); 
 
+    lookBookMenu.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            menDropDown.classList.remove("mens_dropdown_menu_show");
+            womensMenu.classList.remove("womens_dropdown_menu_show");
+            localStoresMenu.classList.remove("local_stores_dropdown_menu");
+            lookBookDropDown.classList.toggle("look_book_dropdown_menu_show");
+                }); 
 
 
 window.addEventListener("keydown", function (evt) {
