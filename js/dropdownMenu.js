a07a03cb5ff7bc60ab0112777629be8856cdddcb
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2020-08-28 19:06:26 +0300

    sign in page finished

--- js/dropdownMenu.js
+++ js/dropdownMenu.js
@@ -1,11 +1,9 @@
 var mensMenu = document.querySelector(".mens_menu");
 var womensMenu = document.querySelector(".womens_menu");
-var localStoresMenu = document.querySelector(".local_stores_menu");
 var lookBookMenu = document.querySelector(".look_book_menu");
 
 var menDropDown = document.querySelector(".mens_dropdown_menu");
 var womenDropDown = document.querySelector(".womens_dropdown_menu");
-var localStoresDropDown = document.querySelector(".local_stores_dropdown_menu");
 var lookBookDropDown = document.querySelector(".look_book_dropdown_menu");
 
 
@@ -13,9 +11,7 @@ var lookBookDropDown = document.querySelector(".look_book_dropdown_menu");
 
 mensMenu.addEventListener("click", function (evt) {
     evt.preventDefault();
-    localStoresDropDown.classList.remove("local_stores_dropdown_menu_show");
     womensMenu.classList.remove("womens_dropdown_menu_show");
-    localStoresMenu.classList.remove("local_stores_dropdown_menu");
     menDropDown.classList.toggle("mens_dropdown_menu_show");
     }); 
 
@@ -23,27 +19,18 @@ mensMenu.addEventListener("click", function (evt) {
 womensMenu.addEventListener("click", function (evt) {
     evt.preventDefault();
         
-    localStoresDropDown.classList.remove("local_stores_dropdown_menu_show");
     menDropDown.classList.remove("mens_dropdown_menu_show");
-    localStoresMenu.classList.remove("local_stores_dropdown_menu");
     womenDropDown.classList.toggle("womens_dropdown_menu_show");
      
         
     }); 
 
 
-localStoresMenu.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    menDropDown.classList.remove("mens_dropdown_menu_show");
-    womensMenu.classList.remove("womens_dropdown_menu_show");
-    localStoresDropDown.classList.toggle("local_stores_dropdown_menu_show");
-        }); 
 
     lookBookMenu.addEventListener("click", function (evt) {
             evt.preventDefault();
             menDropDown.classList.remove("mens_dropdown_menu_show");
             womensMenu.classList.remove("womens_dropdown_menu_show");
-            localStoresMenu.classList.remove("local_stores_dropdown_menu");
             lookBookDropDown.classList.toggle("look_book_dropdown_menu_show");
                 }); 
 
@@ -52,7 +39,7 @@ window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         menDropDown.classList.remove("mens_dropdown_menu_show");
         womensMenu.classList.remove("womens_dropdown_menu_show");   
-        localStoresDropDown.classList.remove("local_stores_dropdown_menu_show");   
+           
     }
 
                 }); 
