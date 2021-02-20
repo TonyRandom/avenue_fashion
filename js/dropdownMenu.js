3f8c50cf08724fc21cf284f0c27588967a0acb20
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-02-20 16:25:08 +0300

    menu finished

--- js/dropdownMenu.js
+++ js/dropdownMenu.js
@@ -2,6 +2,7 @@ var mensMenu = document.querySelector(".mens_menu");
 var womensMenu = document.querySelector(".womens_menu");
 var lookBookMenu = document.querySelector(".look_book_menu");
 
+var ESCAPE_BUTTON = 27;
 
 //меню
 var menDropDown = document.querySelector(".mens_dropdown_menu");
@@ -10,40 +11,64 @@ var lookBookDropDown = document.querySelector(".look_book_dropdown_menu");
 
 var dropdownMenusBtn = document.querySelectorAll(".main_navigation > li");
 var dropdownMenusContainer = document.querySelector(".main_navigation");
-//dropdown-menu__all
 var dropdownMenuBlocks = document.querySelectorAll(".dropdown-menu__all");
 
 console.log(dropdownMenuBlocks);
+
 function closeAllMenus() {
     for (let i = 0; i < dropdownMenuBlocks.length; i++) {
         dropdownMenuBlocks[i].classList.remove("menu_show");
-        /* dropdownMenus[i].classList.remove("womens_dropdown_menu_show"); */
 
     }
 
 }
-/* console.log(menDropDown); */
 
-dropdownMenusContainer.addEventListener('click', function (evt) {
+var onEscapeClose = function (evt) {
+    if (evt.keyCode === ESCAPE_BUTTON) {
+        closeAllMenus();
+        document.removeEventListener('keydown', onEscapeClose);
+    }
+}
+
+ var onClickClose = function (evt) {
+    if (evt.target.closest('li').classList.contains('mens_menu') || evt.target.closest('li').classList.contains('mens_menu')  || evt.target.closest('li').classList.contains('look_book_menu') && menDropDown.classList.contains("menu_show") || womenDropDown.classList.contains("menu_show") || lookBookDropDown.classList.contains("menu_show") ) {
+        closeAllMenus();
+        document.removeEventListener('click', onClickClose);
+    }
+}
+ 
+
+
+
+ function openMenu(evt) {
     let target = evt.target.closest('li');
-    /* console.log(target.classList.contains('womens_menu')); */
+    
     if (!target) {
         closeAllMenus();
     } else if (target.classList.contains('mens_menu')) {
         evt.preventDefault();
         closeAllMenus();
-        menDropDown.classList.toggle("menu_show");
+        menDropDown.classList.add("menu_show");
+        return;
     } else if (target.classList.contains('womens_menu')) {
         evt.preventDefault();
         closeAllMenus();
-        womenDropDown.classList.toggle("menu_show");
+        womenDropDown.classList.add("menu_show");
     } else if (target.classList.contains('look_book_menu')) {
         evt.preventDefault();
         closeAllMenus();
-        lookBookDropDown.classList.toggle("menu_show");
+        lookBookDropDown.classList.add("menu_show");
     }
+    
+} 
+
+
+
+//dropdownMenusContainer (document чтобы по клику по любой области закрывалось меню)
+document.addEventListener('click', function (evt) {
+    openMenu(evt);
+    document.addEventListener('keydown', onEscapeClose);
+    document.addEventListener('click', onClickClose);
+    
 
 })
-
-
-//mens_menu не работает для всех жлементов
